d15bacb27c0cf214b0b6068462cd008352fc00df
majid172 <luthfullahhilmajid172@gmail.com>
2025-06-19 01:27:02 +0600

    category images

--- frontend/src/components/Feature.vue
+++ frontend/src/components/Feature.vue
@@ -9,7 +9,7 @@
         <div class="text-center col-lg-4 col-md-6 mb-4" v-for="category in headerStore.categories">
           <div class="d-flex align-items-center p-4 rounded" style="background-color: #1e293b; color: #f1f5f9;">
 
-              <img :src="logo" class="rounded-circle me-3" alt="Category Image" width="50" height="50">
+              <img :src="`/src/assets/${category.image}`" class="rounded-circle me-3" alt="Category Image" width="50" height="50">
               <div class="text-start">
               <router-link :to="{ name: 'subcategories', params: { id: category.id } }">
                 <h5 class="mb-1 fw-semibold text-light">{{ category.name }}</h5>
--- frontend/src/views/users/SubCategoryView.vue
+++ frontend/src/views/users/SubCategoryView.vue
@@ -21,7 +21,7 @@ onMounted(() => {
 
     <div class="text-center col-lg-4 col-md-6 mb-4" v-for="subcategory in subCategoryStore.subCategory" :key="subcategory.id">
       <div class="d-flex align-items-center p-4 rounded" style="background-color: #1e293b; color: #f1f5f9;">
-        <img :src="logo" class="rounded-circle me-3" alt="Category Image" width="50" height="50" />
+        <img :src="`/src/assets/${subcategory.image}`" class="rounded-circle me-3" alt="Category Image" width="50" height="50" />
         <div class="text-start">
           <router-link :to="{ name: 'subcategories', params: { id: subcategory.id } }" class="text-decoration-none">
             <h5 class="mb-1 fw-semibold text-light">{{ subcategory.name }}</h5>
